53c391cf5aff8c909c0c10d3feb9e9aee14e69ed
dont-give-up-tm <143413537+dont-give-up-tm@users.noreply.github.com>
2026-09-12 21:51:40 +0200

    Update rsvp.js

--- api/rsvp.js
+++ api/rsvp.js
@@ -14,7 +14,7 @@ export default async (req, res) => {
     }
 
     // L'URL du webhook Google Apps Script
-    const webhookUrl = 'https://script.google.com/macros/s/AKfycbxtxUv3EfnaAHmseGdakUxqOxPwqIxCX3r4xCWz_a2YlCBpQ1REIXDoRBy1asENIDje/exec';
+    const webhookUrl = 'https://script.google.com/macros/s/AKfycbz_j86V1PRK48Fr5sKgFSGm-7tqQ9ZetsuQaYzwIyKbnY9g_yfCa7KNyO5F7RyHFF5Q/exec';
     
     // Envoie les données au Google Apps Script
     const response = await fetch(webhookUrl, {
